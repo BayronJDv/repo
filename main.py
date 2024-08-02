f02473d7f2a9cfcf9a5011de634ac2bae6d31b04
bayronj <baironjojoa@gmail.com>
2024-08-02 17:38:04 -0500

    ejercicio agenda

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
-import os
-import time
 
-""" manejando archivos """
+agenda = {}
 
-with open('hola.txt','w') as f:
-    f.write('text at the end of the file')
+while True:
+    print("\nAgenda de contactps")
+    print("1. buscar contacto")
+    print("2. añadir contacto")
+    print("3. Eliminar contacto")
+    print("4. Salir")
     
-time.sleep(3)
-os.remove('hola.txt')
+    opcion = int(input("Elija una opción: "))
 
-          
+    match opcion:
+        case 1:
+            if len(agenda) == 0:
+                print("Aun no se han añadido contactos ")
+            else:
+                nombre = input("Introduzca el nombre del contacto: ")
+                if nombre in agenda:
+                    print(f"Contacto: {nombre}")
+                    print(f"Teléfono: {agenda[nombre]}")
+                else:
+                    print("El contacto no se encuentra en la agenda.")
+                
+        case 2:
+            nombre = input("Introduzca el nombre del contacto: ")
+            telefono = input("Introduzca el teléfono del contacto: ")
+            agenda[nombre] = telefono
+            print("Contacto añadido correctamente.")
+            
+        case 3:
+            nombre = input("Introduzca el nombre del contacto a eliminar: ")
+            if nombre in agenda:
+                del agenda[nombre]
+                print("Contacto eliminado correctamente.")
+            else:
+                print("El contacto no se encuentra en la agenda")
+        case 4:
+            break
+        case _:
+            print("Opción inválida. Intente de nuevo.")
+
 
